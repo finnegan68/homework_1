29bbcb6db9b9b526136026146be4196c925f4d4b
Илья <jj221210@gmail.com>
2016-03-02 18:48:53 +0200

    done

---
+++ 1.1.py
@@ -0,0 +1,5 @@
+first_name = input("Enter your first name")
+second_name = input("Enter your second name")
+print(first_name, second_name, sep=" ")
+print(second_name, first_name, sep=", ")
+print(second_name+"\t"+first_name)
